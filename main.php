253e0e855679ebeada9fef0530ca969fcdb31518
myron <myronmax_seelen@live.nl>
2018-01-24 11:50:58 +0100

    First Commit

--- main.php
+++ main.php
@@ -1,33 +1,33 @@
-<?php
-?>
+    <?php
+    ?>
 
-<!doctype html>
-<html lang="nl">
-<head>
+    <!doctype html>
+    <html lang="nl">
+    <head>
 
-    <link rel="stylesheet" href="Style.css" type="text/css">
+        <link rel="stylesheet" href="Style.css" type="text/css">
 
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <title>Overzicht</title>
-</head>
-<body>
+        <title>Overzicht</title>
+    </head>
+    <body>
 
     <div class="overzicht">
-    <table>
-        <tr>
-            <th><h1>Naam:</h1></th>
-            <th><h1>Email:</h1></th>
-        </tr>
-        <tr>
-            <td>Myron</td>
-            <td>m.seelen@leeuwenborgh.nl</td>
-        </tr>
-    </table>
+        <table>
+            <tr>
+                <th><h1>Naam:</h1></th>
+                <th><h1>Email:</h1></th>
+            </tr>
+            <tr>
+                <td>Myron</td>
+                <td>m.seelen@leeuwenborgh.nl</td>
+            </tr>
+        </table>
     </div>
 
-</body>
-</html>
+    </body>
+    </html>
