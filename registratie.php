0838d2b5e50c377cf4a11728ce5d7389dc248a96
myron <myronmax_seelen@live.nl>
2018-01-25 02:08:02 +0100

    First Commit

--- registratie.php
+++ registratie.php
@@ -1,9 +1,12 @@
 <?php
 
+    $random = rand(1000, 3000);
+    Echo "$random";
+
     $servernaam = 'localhost';
     $username = 'root';
     $password = '';
-    $dbname = 'bhvtest';
+    $dbname = 'bhv';
 
     $con = new mysqli($servernaam, $username, $password , $dbname);
 
@@ -12,9 +15,21 @@
         die("Vebinding mislukt:" . $con->connect_error);
     }
 
+    if(isset($_POST['codeverzenden']))
+    {
+        $sql = "UPDATE administratie SET code='$random' WHERE id=1";
+
+        if (mysqli_query($con, $sql))
+        {
+            echo "<br>";
+            Echo "Code Verzonden";
+        }
+    }
+
 ?>
 
 <!doctype html>
+<html lang="nl">
 <head>
 
     <link rel="stylesheet" href="Style.css" type="text/css">
@@ -24,16 +39,16 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <title>Index</title>
+    <title>Code verzenden</title>
 
 </head>
 <body>
 
+<form action="registratie.php" method="POST">
 <div class="inlogcode">
-    <label>Code:<br><input type="password" name="code" class="label-normal"></label>
-    <br>
-    <input class="btn-normal" type="submit" name="inloggen" value="Inloggen">
+    <input type="submit" name="codeverzenden" value="Stuur Code" class="btn-normal">
 </div>
+</form>
 
 </body>
 </html>
